980a98f8bd8fdbd5fdfdba5b8fe30d3e3ef8293a
Yulia <yuliabondar18aaaa@gmail.com>
2022-02-20 14:46:35 +0200

    Some UI added

--- script.js
+++ script.js
@@ -1,43 +1,108 @@
+let playerScore = 0;
+let computerScore = 0;
+let playerChoice;
+let computerChoice;
+let title = document.querySelector('h1');
+let info = document.querySelector('.info');
+const modal = document.querySelector('.modal');
+let player = document.querySelector('.player');
+let computer = document.querySelector('.computer');
+
+
 const computerPlay = () => {
     let options = ['Rock', 'Paper', 'Scissors'];
     return options[Math.floor(Math.random() * 3)];
 }
 
-const playerPlay = () => prompt('Enter your weapon');
-
-const whoIsWinner = (playerSelection, computerSelection) => {
-    if (playerSelection === computerSelection) return 2;
-    if (playerSelection === 'Rock') return (computerSelection === 'Scissors');
-    if (playerSelection === 'Paper') return (computerSelection === 'Rock');
-    return (computerSelection === 'Paper');
+const identifyWeaponSign = (weapon) => {
+    return (weapon === 'Rock') ? '✊' :
+        (weapon === 'Paper') ? '✋' :
+        '✌';
 }
 
-const playRound = (playerSelection, computerSelection) => {
-    if (whoIsWinner(playerSelection, computerSelection) === 2) return 'It\'s a tie';
-    return (whoIsWinner(playerSelection, computerSelection)) ? `You won! ${playerSelection} beats ${computerSelection}` : `You lost! ${playerSelection} is beaten by ${computerSelection}`;
+showChosenWeapon = () => {
+    let playerWeapon = document.querySelector('.playerWeapon');
+    let computerWeapon = document.querySelector('.computerWeapon');
+    playerWeapon.textContent = identifyWeaponSign(playerChoice);
+    computerWeapon.textContent = identifyWeaponSign(computerChoice);
 }
 
-const gameResult = (playerScore, computerScore) => {
-    let message = (playerScore > computerScore) ? 'You won!' : 
-        (playerScore < computerScore) ? 'You lost' : 'It\'s tie';
-    return message + '\n' + `Total score is ${playerScore} : ${computerScore}`;
+const findWinner = () => {
+    if (playerChoice === computerChoice) return 2;
+    if (playerChoice === 'Rock') return (computerChoice === 'Scissors');
+    if (playerChoice === 'Paper') return (computerChoice === 'Rock');
+    return (computerChoice === 'Paper');
 }
 
-const game = () => {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {    
-        let computerSelection = computerPlay();
-        let playerSelection =  playerPlay();
-        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-        if(whoIsWinner(playerSelection, computerSelection) === 2){
-            playerScore++;
-            computerScore++;
-        } else if(whoIsWinner(playerSelection, computerSelection)) playerScore++;
-        else computerScore++;
-        alert(playRound(playerSelection, computerSelection) + '\n' + `Current score is ${playerScore} : ${computerScore}` );
-    } 
-    alert(gameResult(playerScore, computerScore));
+const showRoundResult = () => {
+    const whoIsWinner = findWinner();
+    if (whoIsWinner === 2){
+        title.textContent =  'It\'s a tie';
+        info.textContent = `${playerChoice} ties with ${computerChoice}`;
+    } else if (whoIsWinner){
+        title.textContent =  'You won!';
+        info.textContent =  `${playerChoice} beats ${computerChoice}`
+    } else {
+        title.textContent =  'You lost!';
+        info.textContent = `${playerChoice} is beaten by ${computerChoice}`;
+    }
 }
 
-game();
+const countScore = () => {
+    const whoIsWinner = findWinner();
+    if (whoIsWinner === 2){
+        playerScore++;
+        computerScore++;
+    } else if (whoIsWinner){
+        playerScore++;
+    } else computerScore++;
+}
+
+const showRoundScore = () => {
+    player.textContent = playerScore;
+    computer.textContent = computerScore;
+}
+
+const playRound = (e) => {
+    if (!e.target.closest('button')) return;
+    const chosenButton = e.target.closest('button');
+    playerChoice = chosenButton.dataset.weapon;
+    computerChoice = computerPlay();
+    showChosenWeapon();
+    showRoundResult();
+    countScore();
+    showRoundScore();
+}
+
+const showModal = () => {
+    const modalResult = document.querySelector('.modalResult');
+    const modalScore = document.querySelector('.modalScore');
+    modalResult.textContent = (playerScore > computerScore) ? 'You won' : 'You lost';
+    modalScore.textContent = `${playerScore} : ${computerScore}`;
+    modal.classList.add('active');
+    const restartButton = document.querySelector('.restartBtn');
+    restartButton.addEventListener('click', restart);
+}
+
+const restart = () => {
+    modal.classList.remove('active');
+    playerScore = 0;
+    computerScore = 0;
+    title.textContent =  'Choose your weapon';
+    info.textContent = 'First to score 5 points wins the game';
+    player.textContent = playerScore;
+    computer.textContent = computerScore;
+    buttons.addEventListener('click', playGame);
+}
+
+const playGame = (e) => {
+    playRound(e);
+    if (playerScore === 5 || computerScore === 5){
+        buttons.removeEventListener('click', playGame);
+        showModal();
+    }
+}  
+
+const buttons = document.querySelector('.buttons');   
+buttons.addEventListener('click', playGame);
+
